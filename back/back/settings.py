cd01fc3aef3572dfb70115be64b731520c57a279
vaniakazeko <vania.kazeko.00@bk.ru>
2022-12-09 16:06:06 +0300

    adding a Profile app and creating a Profile model

--- back/back/settings.py
+++ back/back/settings.py
@@ -15,18 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8$9#5@ub327&8ect$5nd-n3tfogvkx5_bw#&ds7@lcj@zik_1d'
+SECRET_KEY = 'django-insecure-2gk@3tu^f%sh1&qqrf5r%dys67si2ex-wk@p!yrt)nzht4&v27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -38,10 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'src.profiles.apps.ProfilesConfig',
+    'posts.apps.PostsConfig',
+    'profiles.apps.ProfilesConfig',
 
-    'rest_framework',
-    'djoser',
     'debug_toolbar',
 ]
 
@@ -77,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'back.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -91,7 +87,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -111,18 +106,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-Ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -140,16 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    '127.0.0.1',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
-}
-
-SIMPLE_JWT = {
-   # 'AUTH_HEADER_TYPES': ('JWT',),
-}
-
---
+++ back/social network.md
@@ -0,0 +1,17 @@
+social network
+---
+
+1. profiles
+   1. Profile
+   2. Relationship
+
+2. posts
+   1. Post
+   2. Comment
+   3. Like
+   4. 
+
+3. allauth(authentication)
+
+
+
